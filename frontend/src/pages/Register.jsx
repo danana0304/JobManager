fbb624538cdf2df4a59bdea308f4aa70fbc8a466
mgotceitas <93795674+mgotceitas@users.noreply.github.com>
2026-09-01 11:25:56 -0400

    Fixed URLs on JSX files

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -17,7 +17,7 @@ function Register() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
         method: "POST",
 
         headers: {
